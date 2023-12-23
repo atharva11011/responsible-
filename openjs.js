b00a7cd86fa8e70021b07ad39e22a99e133df65e
atharva11011 <154664262+atharva11011@users.noreply.github.com>
2023-12-23 14:27:58 +0530

    upload all file

---
+++ openjs.js
@@ -0,0 +1,38 @@
+
+let countEl =document.getElementById("count-el")
+console.log("countEl")
+let count = 0
+function increment(){
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+let counntEl =document.getElementById("count-el")
+console.log("countEl")
+let counnt = 0
+function decrement(){
+    count = count - 1
+    countEl.innerText = count
+    console.log(count)
+}
+let counnntEl =document.getElementById("count-el")
+console.log("countEl")
+let counnnt   = 0
+count  = count = 0
+function reset11(){
+    document.getElementById("count-el")
+}
+
+ 
+//  const resetButton = document.getElementById('resetButton');
+
+//  // Get the reference to the input field element
+//  const nameInput = document.getElementById('count-el');
+// //  const nameInput = document.getElementById('button-1');
+
+ 
+//  resetButton.addEventListener('click', function() {
+     
+//      nameInput.value = '';
+//  });
